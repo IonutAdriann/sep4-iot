2e825adae89a126ddd1433cbab45b76fa79b3e17
alexione <Alexandru.AlexandruIonescu@gmail.com>
2022-05-03 12:53:37 +0200

    temperature and humidity sensor

---
+++ src/headers/DataShare.h
@@ -0,0 +1,14 @@
+/*
+ * DataShare.h
+ *
+ * Created: 5/2/2022 1:08:19 PM
+ *  Author: alexa
+ */ 
+
+#ifndef datashare_h
+#define datashare_h
+
+extern float temperature;
+extern float humidity;
+
+#endif
---
+++ src/tasks/tempHumid.c
@@ -0,0 +1,58 @@
+/*
+ * tempHumid.c
+ *
+ * Created: 5/2/2022 1:00:29 PM
+ *  Author: alexandru
+ */ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <hih8120.h>
+#include "../headers/tempHumid.h"
+#include "../headers/DataShare.h"
+
+TickType_t xLastWakeTime;
+TickType_t xFrequency;
+
+void TempHumid_taskRun(void)
+{
+	xTaskDelayUntil( &xLastWakeTime, xFrequency);
+	
+	if (HIH8120_OK != hih8120_wakeup())
+	{
+		vTaskDelay(pdMS_TO_TICKS(100))
+		printf("Temperature&humidity sensor didn't wake up retrying");
+		while (HIH8120_OK != hih8120_wakeup())
+		{
+			vTaskDelay(pdMS_TO_TICKS(100));
+		}
+	}
+	
+	vTaskDelay(pdMS_TO_TICKS(500));
+	
+	temperature = hih8120_getTemperature();
+	humidity = hih8120_getHumidity();
+} 
+
+void TempHum_init() 
+{
+	if (HIH8120_OK == hih8120_initialise())
+	{
+		//The driver is initialised
+	} 
+	else
+	{
+		printf("The driver didn't start");
+	}
+}
+
+void TempHumid_getDataFromSensorTask(void *pvParameters)
+{
+	xFrequency = 5000/portTICK_PERIOD_MS;
+	
+	xLastWakeTime = xTaskGetTickCount();
+	
+	for(;;)
+	{
+		TempHumid_taskRun();
+	}
+}
